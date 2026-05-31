30e4377ccd2a777b8351f8a8718a6670e5b09e31
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-31 19:14:09 +0700

    r

--- src/mods/secp256k1/mod.ts
+++ src/mods/secp256k1/mod.ts
@@ -4,14 +4,18 @@ await secp256k1Wasm.load()
 
 export class SecretKey {
 
+  /**
+   * Do not use
+   * @param inner 
+   */
   constructor(
     readonly inner: secp256k1Wasm.Secp256k1SigningKey
   ) { }
 
-  [Symbol.dispose]() {
-    this.inner[Symbol.dispose]()
-  }
-
+  /**
+   * Generate a random secret key
+   * @returns 
+   */
   static random(): SecretKey {
     const { Secp256k1SigningKey } = secp256k1Wasm
 
@@ -20,26 +24,44 @@ export class SecretKey {
     return new SecretKey(inner)
   }
 
-  static import(bytes: Uint8Array): SecretKey {
+  /**
+   * Import from 32 bytes
+   * @param key 
+   * @returns 
+   */
+  static import(key: Uint8Array): SecretKey {
     const { Memory, Secp256k1SigningKey } = secp256k1Wasm
 
-    const inner = Secp256k1SigningKey.from_bytes(new Memory(bytes))
+    const inner = Secp256k1SigningKey.from_bytes(new Memory(key))
 
     return new SecretKey(inner)
   }
 
+  /**
+   * Export to 32 bytes
+   * @returns 
+   */
   export(): Uint8Array {
     return this.inner.to_bytes().bytes
   }
 
+  /**
+   * Publish this secret key to a public key
+   * @returns 
+   */
   publish(): PublicKey {
     return new PublicKey(this.inner.publish())
   }
 
-  sign(message: Uint8Array): Signature {
+  /**
+   * Sign a message hash (32 bytes)
+   * @param hashed 
+   * @returns 
+   */
+  sign(hashed: Uint8Array): Signature {
     const { Memory } = secp256k1Wasm
 
-    const result = this.inner.sign_prehash_recoverable(new Memory(message))
+    const result = this.inner.sign_prehash_recoverable(new Memory(hashed))
 
     return new Signature(result)
   }
@@ -52,18 +74,25 @@ export class PublicKey {
     readonly inner: secp256k1Wasm.Secp256k1VerifyingKey
   ) { }
 
-  [Symbol.dispose]() {
-    this.inner[Symbol.dispose]()
-  }
-
-  static import(bytes: Uint8Array): PublicKey {
+  /**
+   * Import from compressed (33 bytes) or uncompressed (65 bytes) SEC1 format
+   * @param key 
+   * @returns 
+   */
+  static import(key: Uint8Array): PublicKey {
     const { Memory, Secp256k1VerifyingKey } = secp256k1Wasm
 
-    const inner = Secp256k1VerifyingKey.from_sec1_bytes(new Memory(bytes))
+    const inner = Secp256k1VerifyingKey.from_sec1_bytes(new Memory(key))
 
     return new PublicKey(inner)
   }
 
+  /**
+   * Recover a public key from a signature and the message hash (32 bytes)
+   * @param hashed 
+   * @param signature 
+   * @returns 
+   */
   static recover(hashed: Uint8Array, signature: Signature): PublicKey {
     const { Memory, Secp256k1VerifyingKey } = secp256k1Wasm
 
@@ -72,10 +101,19 @@ export class PublicKey {
     return new PublicKey(result)
   }
 
+  /**
+   * Downcast this public key to a point
+   * @returns 
+   */
   downcast() {
     return new Point(this.inner.to_point())
   }
 
+  /**
+   * Export to compressed (33 bytes) or uncompressed (65 bytes) SEC1 format
+   * @param compressed 
+   * @returns 
+   */
   export(compressed: boolean): Uint8Array {
     if (compressed) {
       return this.inner.to_sec1_compressed_bytes().bytes
@@ -88,22 +126,31 @@ export class PublicKey {
 
 export class Signature {
 
+  /**
+   * Do not use
+   * @param inner 
+   */
   constructor(
     readonly inner: secp256k1Wasm.Secp256k1SignatureAndRecovery
   ) { }
 
-  [Symbol.dispose]() {
-    this.inner[Symbol.dispose]()
-  }
-
-  static import(bytes: Uint8Array): Signature {
+  /**
+   * Import from RSV format (32 + 32 + 1 = 65 bytes)
+   * @param rsv 
+   * @returns 
+   */
+  static import(rsv: Uint8Array): Signature {
     const { Memory, Secp256k1SignatureAndRecovery } = secp256k1Wasm
 
-    const inner = Secp256k1SignatureAndRecovery.from_rsv_bytes(new Memory(bytes))
+    const inner = Secp256k1SignatureAndRecovery.from_rsv_bytes(new Memory(rsv))
 
     return new Signature(inner)
   }
 
+  /**
+   * Export to RSV format (32 + 32 + 1 = 65 bytes)
+   * @returns 
+   */
   export(): Uint8Array {
     return this.inner.to_rsv_bytes().bytes
   }
@@ -112,14 +159,17 @@ export class Signature {
 
 export class Point {
 
+  /**
+   * Do not use
+   * @param inner 
+   */
   constructor(
     readonly inner: secp256k1Wasm.Secp256k1Point
   ) { }
 
-  [Symbol.dispose]() {
-    this.inner[Symbol.dispose]()
-  }
-
+  /**
+   * The generator point
+   */
   static get generator(): Point {
     const { Secp256k1Point } = secp256k1Wasm
 
@@ -128,10 +178,17 @@ export class Point {
     return new Point(inner)
   }
 
+  /**
+   * True if this point is the identity point (aka point at infinity, aka zero point, aka neutral element)
+   */
   get identity(): boolean {
     return this.inner.is_identity()
   }
 
+  /**
+   * Upcast this point to a public key
+   * @returns 
+   */
   upcast(): PublicKey {
     const { Secp256k1VerifyingKey } = secp256k1Wasm
 
@@ -140,6 +197,11 @@ export class Point {
     return new PublicKey(inner)
   }
 
+  /**
+   * Multiply this point by a scalar (32 bytes)
+   * @param scalar 
+   * @returns 
+   */
   mul(scalar: Uint8Array): Point {
     const { Memory, Secp256k1Scalar } = secp256k1Wasm
 
@@ -149,6 +211,11 @@ export class Point {
     return new Point(x)
   }
 
+  /**
+   * Add another point to this point
+   * @param other 
+   * @returns 
+   */
   add(other: Point): Point {
     return new Point(this.inner.add(other.inner))
   }
